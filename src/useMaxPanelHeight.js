6c6762f6af7f6727f2589a27dae3e513606f32bd
Craig Spara <craigspara@users.noreply.github.com>
2021-10-15 15:01:31 -0400

    chore: add jsDoc comments

--- src/useMaxPanelHeight.js
+++ src/useMaxPanelHeight.js
@@ -1,30 +1,51 @@
 import { useEffect, useRef, useState } from "react";
 
+/**
+ * @description Utility to set the max-block-size of a panel like those found
+ * in a combobox based on the visual viewport. Visual viewport size changes when
+ * user open the mobile keyboard by focusing on the combobox input
+ *
+ * @returns {object.<object|function|number>}
+ */
 export const useMaxPanelHeight = () => {
   const panelRef = useRef(null);
   const [visualViewportHeight, setVisualViewportHeight] = useState(
     Math.round(window.visualViewport.height)
   );
 
+  /**
+   * @returns {number}
+   */
   const getPanelHeight = () => {
     return panelRef?.current?.offsetHeight;
   };
 
-  const getVisualViewportHeight = () =>
-    setVisualViewportHeight(Math.round(window?.visualViewport?.height));
+  /**
+   * @returns {number}
+   */
+  const getVisualViewportHeight = () => visualViewportHeight;
 
-  function handleVisualViewportResize() {
-    console.log("Handling visualViewportResize");
-
+  /**
+   * @description Handles visual Viewport resize event
+   * @return {undefined}
+   */
+  const handleVisualViewportResize = () => {
     setVisualViewportHeight(Math.round(window?.visualViewport?.height));
     setMaxpanelHeight();
-  }
+  };
+
+  /**
+   * @returns {number}
+   */
   const getMaxPanelHeight = () => {
     const visualViewport = window?.visualViewport;
 
     // Eventually get directly from CSS to avoid magic number
     const arbitraryBlockMarginEndValue = 16;
 
+    /**
+     * @returns {number}
+     */
     const getAvailableSpaceForPanel = () => {
       return (
         visualViewport?.height -
@@ -35,24 +56,20 @@ export const useMaxPanelHeight = () => {
 
     const availableSpaceForPanel = getAvailableSpaceForPanel();
 
-    /* 
-    // eslint-disable-next-line no-console
-    console.groupCollapsed("useMaxPanelBlockSize");
-    console.log("panelRef", panelRef);
-    // eslint-disable-next-line no-console
-    console.log("availableSpaceForPanel", availableSpaceForPanel);
-    // eslint-disable-next-line no-console
-    console.groupEnd();
-    */
-
     return availableSpaceForPanel;
   };
 
+  // TODO: Add check for --panel-max-block-size. If missing, add it with a default value.
+  /**
+   * @description Sets the css custom property, --panel-max-block-size,
+   * to the max-block-size available for the panel
+   * @returns {undefined}
+   */
   const setMaxpanelHeight = () => {
     return (
       panelRef &&
       panelRef?.current?.style.setProperty(
-        "max-block-size",
+        "--panel-max-block-size",
         `${getMaxPanelHeight()}px`
       )
     );
