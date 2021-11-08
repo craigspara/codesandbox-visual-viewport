8b256ac50176416e38994edd65cee739972203b3
cspara <cspara@ebsco.com>
2021-11-08 11:32:59 -0500

    feat: add getPanelBlockMarginEndValue()

--- src/useMaxPanelHeight.js
+++ src/useMaxPanelHeight.js
@@ -4,11 +4,10 @@ import { useEffect, useRef } from "react";
  * @description Utility to set the max-block-size of a panel like those found
  * in a combobox based on the visual viewport. Visual viewport size changes when
  * user open the mobile keyboard by focusing on the combobox input
- *
  *  @param {number} panelBlockMarginEndValue The value of the space between the cross-axis end side of the panel ond the bottom of the visual viewport.
  * @returns {object}
  */
-export const useMaxPanelHeight = (panelBlockMarginEndValue = 16) => {
+export const useMaxPanelHeight = (panelBlockMarginEndValue) => {
   const panelRef = useRef(null);
 
   /**
@@ -20,25 +19,50 @@ export const useMaxPanelHeight = (panelBlockMarginEndValue = 16) => {
   };
 
   /**
-   * @description Determines the vertical space available within the visual viewport that the panel can fit in without extending beyond the bottom of the visual viewport.
+   * @description The value of the space between the cross-axis end side of the panel ond the bottom of the visual viewport.
+   *
+   * @constant
+   * @default 16
+   *
+   * Returns panelBlockMarginEndValue from props if provided
+   * If not, then uses css var --panel-block-margin-end-value
+   * If neither are defined, returns default value of 16
    * @returns {number}
+   */
+  const getPanelBlockMarginEndValue = () => {
+    const defaultValue = 16;
+
+    if (panelBlockMarginEndValue) {
+      return panelBlockMarginEndValue
+    }
+
+    const documentBodyStyles = getComputedStyle(document.documentElement);
+    const cssPanelBlockMarginEndValue =  documentBodyStyles.getPropertyValue('--panel-block-margin-end-value');
+
+    return cssPanelBlockMarginEndValue || defaultValue;
+  };
+
+  /**
+   * @description Determines the vertical space available within the visual viewport that the panel can fit in without extending beyond the bottom of the visual viewport.
+   * @returns {number | string}
    *
    * iOS 15 has a bug when the address bar is at the bottom of the screen and the mobile keyboard is open. The VisualViewport is incorrectly not including the height of the floating address bar. This causes the floating address bar to overlay the last 30(ish) pixels of the  open panel.
    */
   const getMaxPanelHeight = () => {
     const visualViewportHeight = window?.visualViewport?.height;
-    const panelHeight = panelRef?.current?.offsetTop
+    const panelBlockMarginEndValue = getPanelBlockMarginEndValue();
+    // getBoundingClientRect() provides position relative to the viewport. offsetTop provides position relative to the closest positioned parent (offsetParent) element. In a complex layout, these may not be the same value.
+    const panelOffsetFromViewport = panelRef?.current?.getBoundingClientRect().top || 0;
 
     return Math.round(visualViewportHeight -
         panelBlockMarginEndValue -
-        panelHeight
+        panelOffsetFromViewport
         );
   };
 
-  // TODO: Add check for --panel-max-block-size. If missing, add it with a default value.
   /**
    * @description Sets the css custom property, --panel-max-block-size,
-   * to the max-block-size available for the panel
+   * to the max-block-size available for the panel.
    * @returns {undefined}
    */
   const setMaxPanelHeight = () => {
@@ -60,11 +84,8 @@ export const useMaxPanelHeight = (panelBlockMarginEndValue = 16) => {
   });
 
   useEffect(() => {
-    console.count("Initial load useEffect");
-    console.log("offsetTop", panelRef?.current?.offsetTop);
-
     setMaxPanelHeight();
-  }, []);
+  });
 
   return panelRef;
 };
